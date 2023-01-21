715f78f5ce661fd986dc3073f841d33cfed01e95
zsakvo <gdwqsp@gmail.com>
2023-01-21 08:45:21 +0800

    fix config page

--- lib/screens/config/config_screen.dart
+++ lib/screens/config/config_screen.dart
@@ -31,12 +31,93 @@ class _ConfigScreenState extends ConsumerState<ConfigScreen> {
           children: [
             ContentArea(
               builder: (context, scrollController) {
-                return ListView(
-                  children: [
-                    Row(
-                      children: [Text("开机自启动")],
-                    )
-                  ],
+                return Padding(
+                  padding: EdgeInsets.symmetric(vertical: 14, horizontal: 18),
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("开机自启动"),
+                            Transform.translate(
+                              offset: const Offset(12, 0),
+                              child: Transform.scale(
+                                scaleX: 0.6,
+                                scaleY: 0.6,
+                                child: MacosSwitch(
+                                  value: true,
+                                  onChanged: (value) {
+                                    // setState(() => selected = value);
+                                  },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("复制成功弹出通知"),
+                            Transform.translate(
+                              offset: const Offset(12, 0),
+                              child: Transform.scale(
+                                scaleX: 0.6,
+                                scaleY: 0.6,
+                                child: MacosSwitch(
+                                  value: true,
+                                  onChanged: (value) {
+                                    // setState(() => selected = value);
+                                  },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("隐藏Dock图标"),
+                            Transform.translate(
+                              offset: const Offset(12, 0),
+                              child: Transform.scale(
+                                scaleX: 0.6,
+                                scaleY: 0.6,
+                                child: MacosSwitch(
+                                  value: true,
+                                  onChanged: (value) {
+                                    // setState(() => selected = value);
+                                  },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const Divider(),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("关于应用"),
+                            Text(
+                              "基于 Flutter 构建， MacOS 样式的二步验证工具",
+                              style: TextStyle(fontSize: 13, height: 1.8, color: Colors.grey[500]),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             )
